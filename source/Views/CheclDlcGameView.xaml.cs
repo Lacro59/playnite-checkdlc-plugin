5b5eaafe2e23189b814424c032527053f0b309d5
Lacro59 <lacro59@gmail.com>
2021-12-31 13:36:05 +0100

    dlc view ui tweaks

--- source/Views/CheclDlcGameView.xaml.cs
+++ source/Views/CheclDlcGameView.xaml.cs
@@ -15,9 +15,13 @@ namespace CheckDlc.Views
     {
         private CheckDlcDatabase PluginDatabase = CheckDlc.PluginDatabase;
 
+        private Game GameContext;
+
 
         public CheclDlcGameView(Game GameContext)
         {
+            this.GameContext = GameContext;
+
             InitializeComponent();
 
             GameDlc gameDlc = PluginDatabase.Get(GameContext, true);
@@ -32,6 +36,17 @@ namespace CheckDlc.Views
             {
                 Process.Start((string)((FrameworkElement)sender).Tag);
             }
+        }
+
+
+        private void Button_Click_Refresh(object sender, RoutedEventArgs e)
+        {
+            PART_Dlcs.ItemsSource = null;
+            PluginDatabase.Refresh(GameContext.Id);
+
+            GameDlc gameDlc = PluginDatabase.Get(GameContext, true);
+            gameDlc.Items.Sort((x, y) => (y.Name).CompareTo(x.Name));
+            PART_Dlcs.ItemsSource = gameDlc.Items;
         }
     }
 }
